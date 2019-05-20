43996b383d68c4e38398bdc8442c71c4375ef6b4
Egojose <josemicrobiologo@gmail.com>
2019-05-20 14:57:52 -0500

    se agregan imagenes de header y fondo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CrearRegistroComponent } from './crear-registro/crear-registro.component';
 import { EditarRegistroComponent } from './editar-registro/editar-registro.component';
+import { VistaInicioComponent } from './vista-inicio/vista-inicio.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      {path:'',redirectTo:'/vista-inicio',pathMatch:'full'},
+      {path:'',redirectTo:'app-root',pathMatch:'full'},
+      {path:'vista-inicio', component:VistaInicioComponent},
       {path:'crear-registro' , component:CrearRegistroComponent},
       {path:'editar-registro', component:EditarRegistroComponent}
     ])
--- src/app/vista-inicio/vista-inicio.component.ts
+++ src/app/vista-inicio/vista-inicio.component.ts
@@ -10,6 +10,8 @@ export class VistaInicioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+
   }
 
 }
