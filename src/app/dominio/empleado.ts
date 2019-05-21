8c44aa191988ca9ac4619a75e87f50f04658d877
Egojose <josemicrobiologo@gmail.com>
2019-05-21 15:44:53 -0500

    se configura el dominio empleado

--- src/app/dominio/empleado.ts
+++ src/app/dominio/empleado.ts
@@ -1,9 +1,52 @@
 export class Empleado {
-    constructor() {}
-    
+    constructor(
+        public nombreCompleto: string,
+        public usuario: any,
+        public primerNombre: string,
+        public segundoNombre: string,
+        public primerApellido: string,
+        public segundoApellido: string,
+        public numeroDocumento: string,
+        public tipoDocumento: string,
+        public fechaIngreso: any,
+        public tipoContrato: string,
+        public cargo: string,
+        public salario: string,
+        public lugarExpedicion: string,
+        public salarioTexto: string,
+        public area: string,
+        public jefe: any,
+        public direccion: string,
+        public celular: string,
+        public sede: string,
+        public extension: string
+
+    ) {}
+
 
     public static fromJson(element: any) {
-        return new Empleado()
+        return new Empleado(
+            element.Title,
+            element.usuario,
+            element.PrimerNombre,
+            element.SegundoNombre,
+            element.PrimerApellido,
+            element.SegundoApellido,
+            element.NumeroDocumento,
+            element.TipoDocumento,
+            element.FechaIngreso,
+            element.TipoContrato,
+            element.Cargo,
+            element.Salario,
+            element.LugarExpedicion,
+            element.SalarioTexto,
+            element.Area,
+            element.Jefe,
+            element.Direccion,
+            element.Celular,
+            element.Sede,
+            element.Extension
+        )
     }
 
     public static fromJsonList(elements: any) {
