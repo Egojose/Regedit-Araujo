29dd62fe44769e601f8a5bb5687ff5add26f00d2
Egojose <josemicrobiologo@gmail.com>
2019-05-28 17:30:57 -0500

    se agrega método para obtener usuario actual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SPServicio } from './servicios/sp-servicio';
 import { Router } from '@angular/router';
 import { Usuario } from './dominio/usuario';
-import { Grupo } from './dominio/grupo'
+import { Grupo } from './dominio/grupo';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
   title = 'crear-editar-empleado';
+  usuario: Usuario;
+  nombreUsuario: string;
+  grupos: Grupo[] = [];
+  PermisosCrearRegistro: boolean;
+
+  public ngOnInit() {
+   this.ObtenerUsuarioActual();
+  }
   
   constructor(private servicio: SPServicio, private router: Router) {
-   
+   this.PermisosCrearRegistro = false;
 
   }
   navegar() {
     this.router.navigate(["/crear-registro"]);
   }
+
+  ObtenerUsuarioActual() {
+    this.servicio.ObtenerUsuarioActual().subscribe(
+      (respuesta) => {
+        this.usuario = new Usuario(respuesta.Title, respuesta.email, respuesta.Id);
+        this.nombreUsuario = this.usuario.nombre;
+        sessionStorage.setItem('usuario', JSON.stringify(this.usuario));
+        this.servicio.ObtenerGruposUsuario(this.usuario.id).subscribe(
+          (respuesta) => {
+            this.grupos = Grupo.fromJsonList(respuesta);
+            this.obtenerParametrosConfiguracion();
+          }, err => {
+            console.log('Error obteniendo grupos de usuario: ' + err);
+          }
+        )
+      }, err => {
+        console.log('Error obteniendo usuario: ' + err);
+      }
+    )
+  }
+
+  obtenerParametrosConfiguracion() {
+
+  }  
 }
