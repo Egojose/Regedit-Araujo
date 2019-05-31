b8bc8e094b25cdc7da08861ba78f623cce9526b3
Egojose <josemicrobiologo@gmail.com>
2019-05-31 15:13:25 -0500

    se agrega metodo para guardar en lista

--- src/app/crear-registro/crear-registro.component.ts
+++ src/app/crear-registro/crear-registro.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { SPServicio } from '../servicios/sp-servicio';
-
+import { Usuario } from '../dominio/usuario';
 @Component({
   selector: 'app-crear-registro',
   templateUrl: './crear-registro.component.html',
@@ -9,6 +9,9 @@ import { SPServicio } from '../servicios/sp-servicio';
 })
 export class CrearRegistroComponent implements OnInit {
 empleadoForm: FormGroup;
+ObjUsuarios: [];
+usuarios: Usuario[] = [];
+
 
   constructor(private fB: FormBuilder, private servicio: SPServicio) { }
 
@@ -23,6 +26,7 @@ empleadoForm: FormGroup;
       tipoDocumento: [''],
       fechaIngreso:[''],
       tipoContrato: [''],
+      terminoContrato: [''],
       cargo: [''],
       salario: [''],
       lugarExpedicion: [''],
@@ -33,11 +37,88 @@ empleadoForm: FormGroup;
       celular: [''],
       sede: [''],
       extension: [''],
+      bono: [''],
+      afp: [''],
+      universidad: [''],
+      carrera: [''],
+      contactoEmergencia: ['']
     })
+    this.servicio.ObtenerTodosLosUsuarios().subscribe(
+      (Usuarios) => {
+        this.ObjUsuarios = Usuarios;
+      })
   }
 
   onSubmit() {
-    console.log(this.empleadoForm.get('primerNombre').value)
+    console.log(this.empleadoForm.value)
+    let usuario = this.empleadoForm.get('usuario').value;
+    let primerNombre = this.empleadoForm.get('primerNombre').value;
+    let segundoNombre = this.empleadoForm.get('segundoNombre').value;
+    let primerApellido = this.empleadoForm.get('primerApellido').value;
+    let segundoApellido = this.empleadoForm.get('segundoApellido').value;
+    let numeroDocumento = this.empleadoForm.get('numeroDocumento').value;
+    let tipoDocumento = this.empleadoForm.get('tipoDocumento').value;
+    let fechaIngreso = this.empleadoForm.get('fechaIngreso').value;
+    let tipoContrato = this.empleadoForm.get('tipoContrato').value;
+    let terminoContrato = this.empleadoForm.get('terminoContrato').value;
+    let cargo = this.empleadoForm.get('cargo').value;
+    let salario = this.empleadoForm.get('salario').value;
+    let luagarExpedicion = this.empleadoForm.get('lugarExpedicion').value;
+    let salarioTexto = this.empleadoForm.get('salarioTexto').value;
+    let area = this.empleadoForm.get('area').value;
+    let jefe = this.empleadoForm.get('jefe').value;
+    let direccion = this.empleadoForm.get('direccion').value;
+    let celular = this.empleadoForm.get('celular').value;
+    let sede = this.empleadoForm.get('sede').value;
+    let extension = this.empleadoForm.get('extension').value;
+    let bono = this.empleadoForm.get('bono').value;
+    let afp = this.empleadoForm.get('afp').value;
+    let universidad = this.empleadoForm.get('universidad').value;
+    let carrera = this.empleadoForm.get('carrera').value;
+    let contactoEmergencia = this.empleadoForm.get('contactoEmergencia').value;
+    let objEmpleado;
+    let salarioIntegral;
+    let SumaSalarioIntegral;
+
+    if(tipoContrato === 'Integral') {
+      SumaSalarioIntegral = salario + bono + afp;
+      salarioIntegral = `${SumaSalarioIntegral}` 
+    }
+    else {
+      salarioIntegral = "";
+    }
+
+    objEmpleado = {
+      usuario: usuario,
+      PrimerNombre: primerNombre,
+      SegundoNombre: segundoNombre,
+      PrimerApellido: primerApellido,
+      SegundoApellido: segundoApellido,
+      NumeroDocumento: numeroDocumento,
+      TipoDocumento: tipoDocumento,
+      FechaIngreso: fechaIngreso,
+      TipoContrato: tipoContrato,
+      Cargo: cargo,
+      Salario: salario,
+      lugarExpedicion: luagarExpedicion,
+      salarioTexto: salarioTexto,
+      Area: area,
+      Jefe: jefe,
+      Direccion: direccion,
+      Celular: celular,
+      Sede: sede,
+      Extension: extension,
+      Bonos: bono,
+      AFP: afp,
+      TerminoContrato: terminoContrato,
+      Carrera: carrera,
+      Universidad: universidad,
+      SalarioIntegral: salarioIntegral,
+      ContactoEmergencia: contactoEmergencia
+    }
+    
   }
+
+  
 
 }
