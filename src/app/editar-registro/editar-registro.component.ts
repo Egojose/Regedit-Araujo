f0ae73c8c77b606ccfcfce8e40116fd2dcb5a010
Egojose <josemicrobiologo@gmail.com>
2019-06-06 07:22:55 -0500

    se comienza a implementar back para editar

--- src/app/editar-registro/editar-registro.component.ts
+++ src/app/editar-registro/editar-registro.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+  import { Component, OnInit } from '@angular/core';
+  import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+  import { SPServicio } from '../servicios/sp-servicio';
+  import { Usuario } from '../dominio/usuario';
+  import { Router } from '@angular/router';
+  import { ToastrManager } from 'ng6-toastr-notifications';
+  import { Form } from 'sp-pnp-js/lib/sharepoint/forms';
+  import { Sede } from '../dominio/sede';
+  import { Area } from '../dominio/area';
+  import { Cargo } from '../dominio/cargo';
+  import { ItemAddResult } from 'sp-pnp-js';
 
-@Component({
-  selector: 'app-editar-registro',
-  templateUrl: './editar-registro.component.html',
-  styleUrls: ['./editar-registro.component.css']
-})
-export class EditarRegistroComponent implements OnInit {
+  @Component({
+    selector: 'app-editar-registro',
+    templateUrl: './editar-registro.component.html',
+    styleUrls: ['./editar-registro.component.css']
+  })
+  export class EditarRegistroComponent implements OnInit {
+  editarEmpleadoForm: Form;
+  usuario: Usuario;
+  usuarioActual: Usuario;
+  emptyManager: boolean;
+  adjuntoHV: any;
+  adjuntoCertificado: any;
+  adjuntoDiplomas: any;
+  sede: Sede[] = [];
+  area: Area[] = [];
+  cargo: Cargo[] = [];
+  // valorUsuarioPorDefecto: string = "Seleccione";
+  dataUsuarios = [
+    {value: 'Seleccione', label : 'Seleccione'}
+  ];
+  counter: number = 0;
 
-  constructor() { }
 
-  ngOnInit() {
+
+    constructor(private fB: FormBuilder, private servicio: SPServicio, private router: Router, public toastr: ToastrManager) { }
+
+    ngOnInit() {
+    }
+
+    private registrarControles () {
+
+    }
+
   }
-
-}
