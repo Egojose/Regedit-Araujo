e7a1c783991c3ca27af43edeaf3d5338dd86d536
Egojose <josemicrobiologo@gmail.com>
2019-12-17 17:56:46 -0500

    configuracion environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  urlRaiz: 'https://enovelsoluciones.sharepoint.com',
-  urlWeb: 'https://enovelsoluciones.sharepoint.com/sites/IntranetAraujo/Gestion-humana',
-  urlRelativa: '/sites/IntranetAraujo/Gestion-humana',
-  urlWebServ: 'https://enovelsoluciones.sharepoint.com/sites/IntranetAraujo/servicios',
+  urlRaiz: 'https://aribasas.sharepoint.com',
+  urlWeb: 'https://aribasas.sharepoint.com/sites/IntranetPruebas/gestion-Humana',
+  urlRelativa: '/sites/IntranetPruebas/gestion-Humana',
+  urlWebServ: 'https://aribasas.sharepoint.com/sites/IntranetPruebas/Servicios/',
   listaEmpleados: 'empleados',
   listaDocumentos: 'Documentos',
   listaSede: 'Sede',
