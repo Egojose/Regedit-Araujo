d31de45571fd47fb4eacfe350789983195438388
Egojose <josemicrobiologo@gmail.com>
2020-08-27 10:21:16 -0500

    cambia lista empleados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   urlWeb: 'https://enovelsoluciones.sharepoint.com/sites/IntranetAraujo/Gestion-humana',
   urlRelativa: '/sites/IntranetAraujo/Gestion-humana',
   urlWebServ: 'https://enovelsoluciones.sharepoint.com/sites/IntranetAraujo/servicios',
-  listaEmpleados: 'empleados',
+  listaEmpleados: 'EmpleadosNueva',
   listaDocumentos: 'Documentos',
   listaSede: 'Sede',
   listaArea: 'Area',
